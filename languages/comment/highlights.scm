beeb79e9b3751ce04471555aef1517e32847cefa
notsaad <saadmazharr@gmail.com>
2026-03-13 16:29:38 -0400

    fix: switch to thedadams fork and fix capture names
    Use thedadams/tree-sitter-comment which has (prefix) and (text) named
    nodes required by the highlight queries. Switch capture names to base
    token types (constant, string, property, keyword) which are themed
    by all Zed themes, ensuring colors actually appear.

--- languages/comment/highlights.scm
+++ languages/comment/highlights.scm
@@ -1,26 +1,43 @@
 ; TODO, WIP, QUESTION — needs attention, in-progress
-(tag
-  (name) @comment.todo
-  (#match? @comment.todo "^(TODO|WIP|QUESTION|QUESTIONS|IDEA|TEMP|TEMPORARY)$"))
-
-; FIXME, BUG, ERROR — broken, must be fixed
-(tag
-  (name) @comment.error
-  (#match? @comment.error "^(FIXME|BUG|ERROR|ISSUE|DEPRECATED)$"))
-
-; HACK, WARNING, WARN — fragile or workaround
-(tag
-  (name) @comment.warning
-  (#match? @comment.warning "^(HACK|WARNING|WARN|XXX)$"))
+((tag
+  (prefix)? @constant.comment.todo.prefix
+  (name) @constant.comment.todo
+  ("(" @constant.comment.todo.bracket
+    (user) @constant.comment.todo.user
+    ")" @constant.comment.todo.bracket)?
+  ((prefix)? @constant.comment.todo.prefix
+   (text)? @constant.comment.todo.text)*)
+  (#match? @constant.comment.todo "^(TODO|WIP|QUESTION|QUESTIONS|IDEA|TEMP|TEMPORARY|\\?)$"))
 
 ; NOTE, INFO, DOCS — informational
-(tag
-  (name) @comment.note
-  (#match? @comment.note "^(NOTE|INFO|DOCS|PERF|OPTIMIZE|PERFORMANCE|SAFETY|INVARIANT)$"))
+((tag
+  (prefix)? @string.comment.info.prefix
+  (name) @string.comment.info
+  ("(" @string.comment.info.bracket
+    (user) @string.comment.info.user
+    ")" @string.comment.info.bracket)?
+  ((prefix)? @string.comment.info.prefix
+   (text)? @string.comment.info.text)*)
+  (#match? @string.comment.info "^(NOTE|INFO|DOCS|PERF|OPTIMIZE|PERFORMANCE|SAFETY|INVARIANT|\\*)$"))
 
-; Username inside parens: FIXME(user):
-(tag
-  (user) @variable)
+; FIXME, BUG, ERROR — broken, must be fixed
+((tag
+  (prefix)? @property.comment.error.prefix
+  (name) @property.comment.error
+  ("(" @property.comment.error.bracket
+    (user) @property.comment.error.user
+    ")" @property.comment.error.bracket)?
+  ((prefix)? @property.comment.error.prefix
+   (text)? @property.comment.error.text)*)
+  (#match? @property.comment.error "^(FIXME|BUG|ERROR|ISSUE|DEPRECATED|!)$"))
 
-; URLs that appear in comments
-(uri) @string.special.url
+; HACK, WARNING, WARN — fragile or workaround
+((tag
+  (prefix)? @keyword.comment.warn.prefix
+  (name) @keyword.comment.warn
+  ("(" @keyword.comment.warn.bracket
+    (user) @keyword.comment.warn.user
+    ")" @keyword.comment.warn.bracket)?
+  ((prefix)? @keyword.comment.warn.prefix
+   (text)? @keyword.comment.warn.text)*)
+  (#match? @keyword.comment.warn "^(HACK|WARNING|WARN|FIX|SAFETY|XXX|#)$"))
